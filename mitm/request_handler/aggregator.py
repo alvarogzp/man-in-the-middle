9fbdba0256a0fee21b3946032ad37ea9065882d6
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-09-11 13:27:52 +0200

    Decouple BaseMitmRequestHandler from MitmSocketAggregator, creating a new base class BaseAggregatorMitmRequestHandler

---
+++ mitm/request_handler/aggregator.py
@@ -0,0 +1,21 @@
+from mitm import BaseMitmRequestHandler
+from mitm.socket_stream_forwarder import SocketStreamForwarder
+
+
+class BaseAggregatorMitmRequestHandler(BaseMitmRequestHandler):
+    def setup_mitm(self, server_socket, client_socket):
+        aggregator = self.get_mitm_socket_aggregator()
+
+        self.forwarder_from_server = SocketStreamForwarder(server_socket, aggregator.server_out_socket).forward()
+        self.forwarder_from_client = SocketStreamForwarder(client_socket, aggregator.client_out_socket).forward()
+        self.forwarder_from_aggregator = SocketStreamForwarder(aggregator.recv_socket,
+                                                               aggregator.send_socket(server_socket, client_socket)
+                                                               ).forward()
+
+    def loop(self):
+        self.forwarder_from_server.wait()
+        self.forwarder_from_client.wait()
+        self.forwarder_from_aggregator.wait()
+
+    def get_mitm_socket_aggregator(self):
+        raise NotImplementedError()
--- mitm/request_handler/base.py
+++ mitm/request_handler/base.py
@@ -2,27 +2,19 @@ import SocketServer
 import socket
 
 from mitm.config import DESTINATION_ENDPOINT
-from mitm.socket_stream_forwarder import SocketStreamForwarder
 
 
 class BaseMitmRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         server_socket = self.connect_to_destination()
-        aggregator = self.get_mitm_socket_aggregator(server_socket, self.request)
-        self.forward(server_socket, self.request, aggregator)
+        self.setup_mitm(server_socket, self.request)
+        self.loop()
 
     def connect_to_destination(self):
         return socket.create_connection(DESTINATION_ENDPOINT)
 
-    def forward(self, server_socket, client_socket, aggregator):
-        forwarder_from_server = SocketStreamForwarder(server_socket, aggregator.server_out_socket).forward()
-        forwarder_from_client = SocketStreamForwarder(client_socket, aggregator.client_out_socket).forward()
-        forwarder_from_aggregator = SocketStreamForwarder(aggregator.recv_socket,
-                                                          aggregator.send_socket(server_socket, client_socket)
-                                                          ).forward()
-        forwarder_from_server.wait()
-        forwarder_from_client.wait()
-        forwarder_from_aggregator.wait()
+    def setup_mitm(self, server_socket, client_socket):
+        raise NotImplementedError()
 
-    def get_mitm_socket_aggregator(self, server_socket, client_socket):
+    def loop(self):
         raise NotImplementedError()
