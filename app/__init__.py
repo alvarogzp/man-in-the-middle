b10ec79c4f28b2401454c77d52b8f55e3d7cf948
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-09-11 13:29:09 +0200

    Update main to use StdoutViewMitmRequestHandler

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,10 @@
 
 from app.config import PUBLIC_ENDPOINT
 from app.servers import SimpleTCPServer
-from mitm.request_handler.samples import StdoutRedirectMitmRequestHandler
-
+from mitm.request_handler.samples import StdoutRedirectMitmRequestHandler, StdoutViewMitmRequestHandler
 
 if __name__ == "__main__":
-    mitm_server = SimpleTCPServer(PUBLIC_ENDPOINT, StdoutRedirectMitmRequestHandler)
+    mitm_server = SimpleTCPServer(PUBLIC_ENDPOINT, StdoutViewMitmRequestHandler)
     mitm_server.start_foreground()
 
     mitm_server.shutdown()
