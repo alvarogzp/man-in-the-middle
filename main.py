80b6275d9851f3f9c20c60696b8cfb38d986ecf3
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-09-19 16:56:39 +0200

    Use more simple imports in main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 
-from app.args import parsed_args
-from mitm import set_destination_endpoint
+import app
+import mitm
 
 
 if __name__ == "__main__":
-    args = parsed_args()
+    args = app.parsed_args()
 
     print "Listening on", args.listen
     print "Connecting to", args.destination
 
-    set_destination_endpoint(args.destination)
+    mitm.set_destination_endpoint(args.destination)
     mitm_server = args.server(args.listen, args.mitm_type)
     mitm_server.start_foreground()
 
